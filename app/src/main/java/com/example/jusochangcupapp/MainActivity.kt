4db45d0aea00a2f9ae8a27d84b937f0712ffbb66
Seok-HeeHan <46621185+Seok-HeeHan@users.noreply.github.com>
2021-08-13 22:53:48 +0900

    Test 4

--- app/src/main/java/com/example/jusochangcupapp/MainActivity.kt
+++ app/src/main/java/com/example/jusochangcupapp/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.jusochangcupapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
 
 class MainActivity : AppCompatActivity() {
+
+    internal lateinit var viewpager : ViewPager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Viewpager = findViewById<ViewPager>(R.id.viewpager)
+
+        val adapter = ViewPagerAdapter(this)
+        viewpager.adapter = adapter
     }
 }
--- app/src/main/java/com/example/jusochangcupapp/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/jusochangcupapp/ViewPagerAdapter.kt
@@ -1,10 +1,13 @@
 package com.example.jusochangcupapp
 
 import android.content.Context
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
 
 
 class ViewPagerAdapter(private  val context : Context) : PagerAdapter(){
@@ -29,9 +32,22 @@ class ViewPagerAdapter(private  val context : Context) : PagerAdapter(){
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
 
         layoutInflater =context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val v = layoutInflater!!.
+        val v = layoutInflater!!.inflate(R.layout.viewpager_activity, root: null)
+        val image = v.findViewById<View>(R.id.imageview) as ImageView
 
-        return super.instantiateItem(container, position)
+        image.setImageResource(Image[position])
+
+        val vp = container as ViewPager
+        vp.addView(v, 0)
+
+        return v
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        val vp = container as ViewPager
+        val v ='object' as View
+        vp.removeView(v)
+
     }
 
 }
