b2492bf38485dcf55cedf54b976309dec327d51e
Seok-HeeHan <46621185+Seok-HeeHan@users.noreply.github.com>
2021-08-13 22:43:36 +0900

    Test 4

--- app/src/main/java/com/example/jusochangcupapp/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/jusochangcupapp/ViewPagerAdapter.kt
@@ -1,16 +1,37 @@
 package com.example.jusochangcupapp
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.viewpager.widget.PagerAdapter
 
 
 class ViewPagerAdapter(private  val context : Context) : PagerAdapter(){
+
+
+    private var layoutInflater : LayoutInflater? = null
     override fun isViewFromObject(view: View, `object`: Any): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        val Image = arrayOf(
+            R.drawable.ai,
+            R.drawable.css,
+            R.drawable.html
+        )
+
+        return view === 'object'
     }
 
     override fun getCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return Image.size
     }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+
+        layoutInflater =context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val v = layoutInflater!!.
+
+        return super.instantiateItem(container, position)
+    }
+
 }
