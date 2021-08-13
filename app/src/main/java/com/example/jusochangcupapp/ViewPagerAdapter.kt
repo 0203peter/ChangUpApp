9211e8eca77b672937862bad98283f2f0b82bc3f
0203peter <0203peter@gmail.com>
2021-08-13 22:31:14 +0900

    first

---
+++ app/src/main/java/com/example/jusochangcupapp/ViewPagerAdapter.kt
@@ -0,0 +1,16 @@
+package com.example.jusochangcupapp
+
+import android.content.Context
+import android.view.View
+import androidx.viewpager.widget.PagerAdapter
+
+
+class ViewPagerAdapter(private  val context : Context) : PagerAdapter(){
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
